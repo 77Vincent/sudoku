ff4638697868040c16943e83a02cc03a987f69e0
Vincent77 <wentianqi77@outlook.com>
2019-09-17 04:55:49 +0800

    stuck

--- sudoku2.js
+++ sudoku2.js
@@ -18,16 +18,23 @@ function random(min, max) {
 }
 
 function doku2(board) {
-  const nums = { 1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [], 8: [], 9: [], } 
-
-  for (let num = 1; num <= 9; num += 1) {
+  for (let su = 1; su <= 9; su += 1) {
     for (let i = 0; i < 9; i += 1) {
       const range = getRange(i + 1)
 
       let r = random(range.r[0], range.r[1])
       let c = random(range.c[0], range.c[1])
 
-      board[r][c] = num
+      while (
+        board[r][c] !== 0
+        || board[r].includes(su)
+        || board.map(row => row[c]).includes(su)
+      ) {
+        r = random(range.r[0], range.r[1])
+        c = random(range.c[0], range.c[1])
+      }
+
+      board[r][c] = su 
     }
   }
   return board
