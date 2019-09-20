cae7af8ca0780722fb6e6a5e4fa46d612125c705
fr-wentianqi <tianqi.wen@fastretailing.com>
2019-09-20 17:51:51 +0800

    rename variables cors to groupedBySu

--- sudoku.js
+++ sudoku.js
@@ -14,11 +14,11 @@ const {
 
 function sudoku(board) {
   board = board || new Array(9).fill().map(() => new Array(9).fill(0))
-  const cors = []
+  const groupedBySu = new Array(9).fill().map(() => new Array(9).fill(0))
   const existing = []
 
   // record the existing ones
-  board.map((row, y) => row.map((num, x) => {
+  board.forEach((row, y) => row.forEach((num, x) => {
     if (num !== 0) {
       existing.push({
         value: num,
@@ -29,13 +29,13 @@ function sudoku(board) {
 
   for (let suIndex = 0; suIndex < 9; suIndex += 1) {
     let su = suIndex + 1
-    cors[suIndex] = []
+    groupedBySu[suIndex] = []
 
     for (let blockIndex = 0; blockIndex < 9; blockIndex += 1) {
       const range = getBlockRange(blockIndex + 1)
       const corRange = multiplyArray(range.x, range.y)
 
-      // Skip blocks where there is already c su filled in
+      // Skip blocks where su is already filled in
       if (corRange.some(cor => board[cor[1]][cor[0]] === su)) {
         continue
       }
@@ -54,9 +54,10 @@ function sudoku(board) {
         board = board.map(row => row.map(num => num === su - 1 ? 0 : num))
         break
       } else {
-        cors[suIndex].push(pick(...availables))
-        const x = cors[suIndex][blockIndex][0]
-        const y = cors[suIndex][blockIndex][1]
+        const cor = pick(...availables)
+        groupedBySu[suIndex].push(cor)
+        const x = groupedBySu[suIndex][blockIndex][0]
+        const y = groupedBySu[suIndex][blockIndex][1]
         board[y][x] = su
       }
     }
